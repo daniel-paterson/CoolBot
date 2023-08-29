f59e4ba96ac324ebbf6035131b7372fdd8a908aa
daniel-paterson <drdragon08@hotmail.com>
2023-08-29 16:22:17 -0700

    removed old audio code and added intents

--- CoolBot.py
+++ CoolBot.py
@@ -1,7 +1,8 @@
-import discord, pafy, asyncio, random
+import discord, random
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '$') #sets up the bot, names it 'bot', and sets the command prefix
+
+bot = commands.Bot(command_prefix = ';', intents = discord.Intents.default()) #sets up the bot, names it 'bot', and sets the command prefix
 
 class WrongChannelError(commands.CheckFailure): #custom error for trying to call the bot in the wrong channel
     pass
@@ -57,73 +58,15 @@ class Misc(commands.Cog):
         
         
 
-class AudioSorce(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        
-    
-    @commands.command() 
-    @in_Bot_Commands()
-    async def getSource(self, ctx, url):
-        try:
-            video = pafy.new(url)
-        except ValueError:
-            await ctx.send("Error: Not a valid link")
-        
-        audio = video.getbestaudio()
-        
-        
-            
 
-class Player(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        
-    @commands.command()
-    @in_Bot_Commands()
-    async def join(self, ctx): #joins the vc that the person who made the command is currently in
-        
-        try:
-            channel = ctx.author.voice.channel
-            
-            if ctx.voice_client is not None:
-                return await ctx.voice_client.move_to(channel)
-
-            await channel.connect()
-            
-        except AttributeError:
-            await ctx.send("Enter the voice chat you want me to join first.")
-        
-    @commands.command()
-    @in_Bot_Commands()
-    async def leave(self, ctx): #leaves the current vc
-        
-        if ctx.voice_client is not None:
-            await ctx.disconnect()
-        else:
-            await ctx.send("I need to be in a voice chat before I can leave one")
-            
-            
-    """@commands.command()
-    @in_Bot_Commands()
-    async def p(self, ctx, *, url): #streams audio from a url
-        
-        
-        async with ctx.typing():
-            
-            
-        await ctx.send('Now playing')"""
-
-
-def getToken():
+def getKey():
     with open("apiKey.txt") as keyFile:
         key = keyFile.readlines()
     return key
 
 
 bot.add_cog(Misc(bot))
-bot.add_cog(AudioSorce(bot))
-bot.add_cog(Player(bot))
 
-bot.run(getToken()) #runs the bot, establishing a link with Discord using the API token. Needs to reman the final line in the script
+
+bot.run(getKey()) #runs the bot, establishing a link with Discord using the API token. Needs to reman the final line in the script
 #TOKEN NEEDS TO REMAIN A SECRET, REGENERATE ASAP IF LEAKED!!!
