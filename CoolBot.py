a23c8ce09f84c966743328588054eeb6cf7b0d5e
daniel-paterson <drdragon08@hotmail.com>
2023-08-29 17:13:43 -0700

    added error handling for not recognising command

--- CoolBot.py
+++ CoolBot.py
@@ -1,5 +1,6 @@
 import discord, random
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 
 def getKey():
     with open("apiKey.txt") as keyFile:
@@ -9,13 +10,25 @@ def getKey():
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = commands.Bot(command_prefix = ';', intents=intents) #sets up the bot, names it 'bot', and sets the command prefix
+#sets up the bot, names it 'bot', and sets the command prefix
+bot = commands.Bot(command_prefix = ';', intents=intents)
 
-@client.event
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+       await ctx.send("Yeah, I don't know what that means")
+
+#prints in the terminal when the bot is online
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user}')
 
 
 
+@bot.command()
+async def test(ctx, *args):
+    userSaid = " ".join(args)
+    await ctx.send(f"You said {userSaid}")
 
-client.run(getKey())
+
+bot.run(getKey())
