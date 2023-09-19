91e508f460318d5cc66af50ccdee4cd2105cc739
daniel-paterson <drdragon08@hotmail.com>
2023-09-19 14:58:23 -0700

    tryingNewCode

--- CoolBot.py
+++ CoolBot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import asyncio
+
 
 from general_cog import general_cog
 from music_cog import music_cog
@@ -12,17 +14,25 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix = ';', intents=intents)
 
-@bot.event
-async def on_ready():
+async def cogLoader():
+    await bot.add_cog(ttrpg_cog(bot))
     await bot.add_cog(general_cog(bot))
     await bot.add_cog(music_cog(bot))
-    await bot.add_cog(ttrpg_cog(bot))
+    print("exiting cogLoader!")
+
+@bot.event
+async def on_ready():
     print(f'Logged in as {bot.user}')
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
        await ctx.send("I don't know what that means")
+
+@bot.command() #useful for bug testing
+async def test(ctx, *args):
+    userSaid = " ".join(args)
+    await ctx.send(f"{ctx.message.author} said {userSaid}")
 
 
 
@@ -31,5 +41,9 @@ def getKey():
     with open("apiKey.txt") as keyFile:
         key = keyFile.readline()
     return key
+
+asyncLoop = asyncio.get_event_loop()
+coroutine = cogLoader()
+asyncLoop.run_until_complete(coroutine)
 
 bot.run(getKey())
--- general_cog.py
+++ general_cog.py
@@ -5,32 +5,8 @@ class general_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.helpMessage = """
-```
-General Commands:
-/help - Displays all avalible commands
 
-Music commands:
-/p <search terms> - Searches for a song and plays it in a voice channel
-/q - Displays the current song queue
-/skip - Skips the currently playing song
-/clear - Stops the current song and clears the queue
-/leave - Kicks the bot from the voice channel
-/pause - Pauses the current song, or resumes it if already paused
-/resume - Resumes the current song
-```
-"""
-
-
-    # @commands.HelpCommand()
-    # async def help(self, ctx, arg):
-    #     if arg == None:
-    #         await ctx.send(self.helpMessage)
-    #     else:
-    #         await ctx.send(ctx.send_command_help(arg))
-    
-
-    @commands.command() #turns off the bot, only usable by me
+    @commands.command() #turns off the bot, only usable by me (maybe)
     async def shutdown(self, ctx):
         if await self.is_owner(ctx.message.author):
             await ctx.send("Logging off...")
@@ -38,7 +14,6 @@ Music commands:
         else:
             await ctx.send("Only the bot owner can use this command") #note to self, figure out the other better way of checking if the owner is sending the message
     
-    @commands.command() #useful for bug testing
-    async def test(self, ctx, *args):
-        userSaid = " ".join(args)
-        await ctx.send(f"{ctx.message.author} said {userSaid}")
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.sent("pong!")
