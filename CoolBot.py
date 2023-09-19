034c4106555906ceebecec0c43d471117feb8423
daniel-paterson <drdragon08@hotmail.com>
2023-09-19 15:16:01 -0700

    fixed bugs, now it kinda starts to run

--- CoolBot.py
+++ CoolBot.py
@@ -1,5 +1,5 @@
 import discord
-from discord import commands
+from discord.ext import commands
 
 class bot(commands.Bot):
     def __init__(self):
@@ -12,21 +12,10 @@ class bot(commands.Bot):
         await self.load_extention("general_cog")
 
 
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-       await ctx.send("I don't know what that means")
-
-
 #for obvious reasons, the api key is not stored on git
 def getKey():
     with open("apiKey.txt") as keyFile:
         key = keyFile.readline()
     return key
 
-bot.run(getKey())
+bot().run(getKey())
