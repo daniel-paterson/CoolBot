081399bd6c570b07aa72fadfa6b2346f69a256b0
daniel-paterson <drdragon08@hotmail.com>
2023-09-19 18:09:55 -0700

    got roll to work

--- ttrpg_cog.py
+++ ttrpg_cog.py
@@ -1,5 +1,26 @@
-import random
+import re
+from random import randint
 from discord.ext import commands
+
+def sanitizeDice(dice):
+        dice = ''.join(dice)
+        dice.replace(" ", "")
+        dice = re.split('[dD+]', dice)
+        for i in range(len(dice)):
+            dice[i] = int(dice[i])
+        return dice
+    
+def calcDice(dice):
+    dice = sanitizeDice(dice)
+    total = 0
+    numbers = []
+    for i in range(dice[0]):
+        ranNum = randint(1, dice[1])
+        numbers.append(ranNum)
+        total += ranNum
+    if len(dice)>2:
+        total += dice[2]
+    return numbers, total
 
 class ttrpgCog(commands.Cog):
     def __init__(self, bot):
@@ -14,28 +35,10 @@ class ttrpgCog(commands.Cog):
     async def cog_unload(self):
         print(f"{self.__class__.__name__} unloaded!")
 
-    
     @commands.command() #used for ttrpgs and other stuff like that. TODO: add support for addition, ie "2d10 + 2" or "1d6 + 3d4 + 2"
-    async def roll(ctx, dice: str):
-
-        try:
-            rolls, limit = map(int, dice.split('d'))
-            if (rolls < 1 or limit < 1):
-                raise Exception
-            if (rolls > 100 or limit > 100):
-                await ctx.send('For the sake of keeping things running smoothly, please don\'t use numbers greater than 100.')
-        except Exception:
-            await ctx.send('Format has to be in NdM where N and M are non-zero integers.')
-            return
-
-        numbers = ''
-        result = 0
-        for r in range(rolls):
-            n = random.randint(1, limit)
-            numbers += (str(n)+ '  ')
-            result += n
-
-        await ctx.send(f'Numbers rolled: {numbers}\nTotal: {result}')
+    async def roll(self, ctx, *args):
+        numbers, total = calcDice(args)
+        await ctx.send(f'Numbers rolled: {numbers}\nTotal: {total}')
 
 async def setup(bot):
     await bot.add_cog(ttrpgCog(bot=bot))
